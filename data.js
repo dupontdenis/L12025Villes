aae1ab6c615db8ec04486ed6b647c0a2f383aa96
denis dupont <denis.dupont@univ-evry.fr>
2025-04-17 10:19:09 +0200

    add towns

--- data.js
+++ data.js
@@ -1,14 +1,28 @@
 const villes = [
   { name: "Avignon", habitants: 140000, region: "Provence-Alpes-Côte d'Azur" },
+  name: "Marseille",
+  habitants: 870000,
+  region: "Provence-Alpes-Côte d'Azur",
+},
+{ name: "Paris", habitants: 2200000, region: "Île-de-France" },
+{ name: "Versailles", habitants: 85000, region: "Île-de-France" },
+{ name: "Annecy", habitants: 130000, region: "Auvergne-Rhône-Alpes" },
+{ name: "Nice", habitants: 340000, region: "Provence-Alpes-Côte d'Azur" },
+{ name: "Toulon", habitants: 170000, region: "Provence-Alpes-Côte d'Azur" },
+{
+  { name: "Grenoble", habitants: 160000, region: "Auvergne-Rhône-Alpes" },
+  { name: "Saint-Étienne", habitants: 175000, region: "Auvergne-Rhône-Alpes" },
+  { name: "Nanterre", habitants: 95000, region: "Île-de-France" },
+  { name: "Boulogne-Billancourt", habitants: 120000, region: "Île-de-France" },
+  { name: "Cannes", habitants: 74000, region: "Provence-Alpes-Côte d'Azur" },
+  { name: "Lyon", habitants: 520000, region: "Auvergne-Rhône-Alpes" },
+  { name: "Valence", habitants: 63000, region: "Auvergne-Rhône-Alpes" },
   {
-    name: "Marseille",
-    habitants: 870000,
+    name: "Aix-en-Provence",
+    habitants: 145000,
     region: "Provence-Alpes-Côte d'Azur",
   },
-  { name: "Lyon", habitants: 520000, region: "Auvergne-Rhône-Alpes" },
-  { name: "Paris", habitants: 2200000, region: "Île-de-France" },
-  { name: "Versailles", habitants: 85000, region: "Île-de-France" },
-  { name: "Annecy", habitants: 130000, region: "Auvergne-Rhône-Alpes" },
+  { name: "Montreuil", habitants: 109000, region: "Île-de-France" },
 ];
 
 export default villes;
